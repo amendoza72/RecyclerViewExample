35befbbefcb044ef1c1b213a0297ac5873db41c8
Antonio Mendoza <antonio.mendoza@gmail.com>
2024-01-11 09:01:39 +0100

    Adapter

--- app/src/main/java/com/example/recyclerviewexample/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewexample/MainActivity.kt
@@ -11,16 +11,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var recyclerViewPlanets: RecyclerView = findViewById(R.id.recyclerViewPlanets)
-
         var planetList = generatePlanets()
     }
 
 
     fun generatePlanets(): ArrayList<Planet>{
         var planets = ArrayList<Planet>()
-
-
-
+        var planet = Planet("Mercurio", "Terrestre", R.drawable.mercurio)
+        planets.add(planet)
+        planet = Planet("Venus", "Terrestre", R.drawable.venus)
+        planets.add(planet)
+        planet = Planet("Tierra", "Terrestre", R.drawable.tierra)
+        planets.add(planet)
+        planet = Planet("Marte", "Terrestre", R.drawable.marte)
+        planets.add(planet)
+        planet = Planet("Jupiter", "Gigante gaseoso", R.drawable.jupiter)
+        planets.add(planet)
+        planet = Planet("Saturno", "Gigante gaseoso", R.drawable.saturno)
+        planets.add(planet)
+        planet = Planet("Urano", "Gigante helado", R.drawable.urano)
+        planets.add(planet)
+        planet = Planet("Neptuno", "Gigante helado", R.drawable.neptuno)
+        planets.add(planet)
         return planets
     }
 }
---
+++ app/src/main/java/com/example/recyclerviewexample/adapters/PlanetAdapter.kt
@@ -0,0 +1,11 @@
+package com.example.recyclerviewexample.adapters
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class PlanetAdapter : RecyclerView.Adapter<PlanetAdapter.PlanetViewHolder>(){
+
+    class PlanetViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+
+    }
+}
