1867c4f387a55a190c2de439571f43504b8726ba
Antonio Mendoza <antonio.mendoza@gmail.com>
2024-01-11 08:42:23 +0100

    Imágenes planetas

--- app/src/main/java/com/example/recyclerviewexample/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewexample/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.recyclerviewexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerviewexample.model.Planet
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var recyclerViewPlanets: RecyclerView = findViewById(R.id.recyclerViewPlanets)
+
+        var planetList = generatePlanets()
+    }
+
+
+    fun generatePlanets(): ArrayList<Planet>{
+        var planets = ArrayList<Planet>()
+
+
+
+        return planets
     }
 }
---
+++ app/src/main/java/com/example/recyclerviewexample/model/Planet.kt
@@ -0,0 +1,3 @@
+package com.example.recyclerviewexample.model
+
+data class Planet (val name:String, val type: String, val image:Int)
