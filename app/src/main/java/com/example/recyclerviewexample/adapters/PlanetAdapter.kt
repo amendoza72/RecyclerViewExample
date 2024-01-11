a7ff9b99e0fc0e06a8d8c782955d7e2353b35db2
Antonio Mendoza <antonio.mendoza@gmail.com>
2024-01-11 09:32:39 +0100

    Adapter generado

--- app/src/main/java/com/example/recyclerviewexample/adapters/PlanetAdapter.kt
+++ app/src/main/java/com/example/recyclerviewexample/adapters/PlanetAdapter.kt
@@ -1,11 +1,48 @@
 package com.example.recyclerviewexample.adapters
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerviewexample.R
+import com.example.recyclerviewexample.model.Planet
 
-class PlanetAdapter : RecyclerView.Adapter<PlanetAdapter.PlanetViewHolder>(){
+class PlanetAdapter(private val planetList: ArrayList<Planet>) : RecyclerView.Adapter<PlanetAdapter.PlanetViewHolder>(){
 
     class PlanetViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+        val textViewName : TextView
+        val textViewType: TextView
+        val imageViewPlanet: ImageView
 
+        init{
+            textViewName = itemView.findViewById(R.id.textViewName)
+            textViewType = itemView.findViewById(R.id.textViewType)
+            imageViewPlanet = itemView.findViewById(R.id.imageViewPlanet)
+        }
+
+        fun bind(planet: Planet){
+            textViewName.text = planet.name
+            textViewType.text = planet.type
+            imageViewPlanet.setImageResource(planet.image)
+        }
     }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlanetViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.planet_item, parent, false)
+        return PlanetViewHolder(view)
+    }
+
+    override fun getItemCount() = planetList.size
+
+    override fun onBindViewHolder(holder: PlanetViewHolder, position: Int) {
+        //holder.textViewName.text = planetList[position].name
+        //holder.textViewType.text = planetList[position].type
+        //holder.imageViewPlanet.setImageResource(planetList[position].image)
+
+        holder.bind(planetList[position])
+    }
+
+
 }
